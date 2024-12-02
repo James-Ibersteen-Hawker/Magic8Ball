4c58f7896c63cd49b871cc7e04512827b397a8a3
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2024-12-02 11:55:12 -0500

    working on canvas

--- canvas.js
+++ canvas.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let time = 0;
-let width = window.innerWidth / 10;
 canvas.setAttribute("width", `${window.innerWidth}px`);
 canvas.setAttribute("height", `${window.innerHeight}px`);
 
@@ -14,6 +13,23 @@ function increment() {
 
 function drop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //rect open
+  table();
+  //arc open
+  ball();
+  //movement open
+  let results = increment();
+  let incr = results[0];
+  let distance = results[1];
+  time += incr;
+  if (time <= Math.round(window.innerHeight - window.innerHeight / 10)) {
+    requestAnimationFrame(drop);
+  } else {
+    return;
+  }
+}
+
+function table() {
   ctx.beginPath();
   ctx.rect(
     0,
@@ -24,19 +40,13 @@ function drop() {
   ctx.fillStyle = "green";
   ctx.fill();
   ctx.stroke();
+}
+function ball() {
+  let width = window.innerWidth / 10;
   let percentage = window.innerWidth - window.innerWidth / 5;
   ctx.beginPath();
-  ctx.arc(percentage, time, width, 0, 2 * Math.PI);
+  ctx.arc(percentage /*xpos*/, time /*ypos*/, width /*radius*/, 0, 2 * Math.PI);
   ctx.stroke();
-  let results = increment();
-  let incr = results[0];
-  let distance = results[1];
-  time += incr;
-  if (time < window.innerHeight - window.innerHeight / 3) {
-    requestAnimationFrame(drop);
-  } else {
-    return;
-  }
 }
 
 drop();
